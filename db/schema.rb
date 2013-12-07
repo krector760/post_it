373b142f1c20a35c1216911f32ad67bf4a9092d4
krector760 <krector760@gmail.com>
2013-12-06 19:51:27 -0800

    Comment,User,Post associations DONE

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206221444) do
+ActiveRecord::Schema.define(version: 20131207032952) do
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -19,6 +27,7 @@ ActiveRecord::Schema.define(version: 20131206221444) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
