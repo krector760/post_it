5b5cda0d00089e737074fe51e02fa77d843c49f8
krector760 <krector760@gmail.com>
2013-12-08 08:28:25 -0800

    adding NEW/EDIT posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,20 +8,46 @@ class PostsController < ApplicationController
   end
 
   def new
-
+  	@post = Post.new
   end
 
   def create
+  	@post = Post.new(post_params)
+
+  		if @post.save
+    	redirect_to @post
+  		else
+    	render 'new'
+  		end
 
   end
 
   def edit
-
+  	@post = Post.find(params[:id])
   end
 
   def update
-
+  	@post = Post.find(params[:id])
+ 
+  	if @post.update(params[:post])
+    redirect_to @post
+  	else
+    render 'edit'
+  	end
   end
-  
+
+  def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to posts_path
+end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+
 
 end
